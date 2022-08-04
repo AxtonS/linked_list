7c7a3a213499b90d58d01c3a9b825c4341afe819
Axton Smith <axtonsmith@gmail.com>
2022-08-04 16:21:32 -0400

    Build size and tail methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -21,6 +21,23 @@ class LinkedList
     pointer = @head
     @head = Node.new(value, next_node = pointer)
   end
+
+  def size
+    counter = 1
+    pointer = @head
+    until pointer.next_node.nil?
+      pointer = pointer.next_node
+      counter += 1
+    end
+    counter
+  end
+
+  def tail
+    pointer = @head
+    pointer = pointer.next_node until pointer.next_node.nil?
+
+    pointer
+  end
 end
 
 # Creates elements to be used in linked lists
@@ -32,3 +49,12 @@ class Node
     @next_node = next_node
   end
 end
+
+test = LinkedList.new(2)
+test.append(3)
+test.prepend(1)
+p test.head.value
+p test.head.next_node.value
+p test.head.next_node.next_node.value
+p test.size
+p test.tail
